d58e0da9703fc74df86fa8d08742983da18145e9
Cody Buffaloe <codylbuffaloe@gmail.com>
2016-04-27 12:24:11 -0400

    Started project 1

---
+++ bubble_sort.rb
@@ -0,0 +1,14 @@
+def bubble_sort(array)
+
+    while(array != array.sort)
+
+        array.each_with_index do | num, index |
+            a = 
+            if()
+
+        end
+    end
+
+end
+
+bubble_sort([9,5,2,6,7,3])
